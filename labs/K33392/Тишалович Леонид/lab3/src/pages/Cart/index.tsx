bc6002cdd2ebf916e696f9bb3a6698ecf0d5416f
Leonid Tishalovich <lenik200320@mail.ru>
2024-01-17 00:02:34 +0300

    fixes: new Cart page

--- labs/K33392/Тишалович Леонид/lab3/src/pages/Cart/index.tsx
+++ labs/K33392/Тишалович Леонид/lab3/src/pages/Cart/index.tsx
@@ -3,23 +3,45 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { ItemData } from "../Main";
 import { Item } from "../../components";
+import axios from "axios";
 
 export const Cart = () => {
   const cartItems = useSelector((state: RootState) => state.cart.items);
+  const totalAmount = cartItems.reduce((total, item) => total + item.price, 0);
+
+  const handleOrder = async () => {
+    const orderData = {
+      items: cartItems.map((item) => item.title),
+      totalAmount,
+    };
+    try {
+      await axios.post("http://localhost:3000/orders", orderData);
+      alert("Order placed successfully:");
+    } catch (error) {
+      console.error("Error placing order:", error);
+    }
+  };
+
   return (
     <div className={style.cartWrapper}>
-      <h1>Cart</h1>
-      <div className={style.itemSelection}>
-        {cartItems.map((item: ItemData) => {
-          const props = {
-            id: item.id,
-            title: item.title,
-            img: item.img,
-            price: item.price,
-            brand: item.brand,
-          };
-          return <Item {...props} />;
-        })}
+      <h1 className={style.cartTitle}>Cart</h1>
+      <div className={style.mainContent}>
+        <div className={style.itemSelection}>
+          {cartItems.map((item: ItemData) => {
+            const props = {
+              id: item.id,
+              title: item.title,
+              img: item.img,
+              price: item.price,
+              brand: item.brand,
+            };
+            return <Item {...props} />;
+          })}
+        </div>
+        <div className={style.total}>
+          <h1>Total: {totalAmount}$</h1>
+          <button onClick={handleOrder}>Order</button>
+        </div>
       </div>
     </div>
   );
